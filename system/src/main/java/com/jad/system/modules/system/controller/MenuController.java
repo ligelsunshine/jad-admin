a65b56a6c0d606639156cf40a7cd029ab5d94577
cxxwl96 <cxxwl96@sina.com>
2021-08-11 22:17:16 +0800

    新增管理接口

--- system/src/main/java/com/jad/system/modules/system/controller/MenuController.java
+++ system/src/main/java/com/jad/system/modules/system/controller/MenuController.java
@@ -7,14 +7,18 @@ package com.jad.system.modules.system.controller;
 import com.jad.common.base.controller.BaseController;
 import com.jad.common.entity.Menu;
 import com.jad.common.enums.MenuType;
+import com.jad.common.exception.BadRequestException;
 import com.jad.common.lang.Result;
 import com.jad.common.service.MenuService;
 import com.jad.common.service.UserService;
 import com.jad.common.utils.ValidatorUtil;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +27,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import cn.hutool.core.util.StrUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -47,21 +52,43 @@ public class MenuController extends BaseController {
     @PostMapping("/save")
     public Result save(@RequestBody @Valid Menu menu) {
         // 分组实体校验
-        Class<?> clazzGroup;
-        if (menu.getType() == MenuType.DIRECTORY) {
-            clazzGroup = Menu.DirectoryValidGroup.class;
-        } else if (menu.getType() == MenuType.MENU) {
-            clazzGroup = Menu.MenuValidGroup.class;
-        } else {
-            clazzGroup = Menu.ButtonValidGroup.class;
-        }
-        ValidatorUtil.validateEntity(menu, clazzGroup);
+        validMenu(menu, false);
 
         final boolean success = menuService.save(menu);
         if (success) {
             return Result.success("添加成功");
         }
         return Result.failed("添加失败");
+    }
+
+    @ApiOperation("删除菜单权限")
+    @DeleteMapping("/delete/{id}")
+    public Result delete(@PathVariable String id) {
+        final boolean success = menuService.removeById(id);
+        if (success) {
+            return Result.success("删除成功");
+        }
+        return Result.failed("删除失败");
+    }
+
+    @ApiOperation("修改菜单权限")
+    @PutMapping("/update")
+    public Result update(@RequestBody @Valid Menu menu) {
+        // 分组实体校验
+        validMenu(menu, true);
+
+        final boolean success = menuService.updateById(menu);
+        if (success) {
+            return Result.success("修改成功");
+        }
+        return Result.failed("修改失败");
+    }
+
+    @ApiOperation("获取单个菜单权限")
+    @GetMapping("/get/{id}")
+    public Result get(@PathVariable String id) {
+        final Menu menu = menuService.getById(id);
+        return Result.success(menu);
     }
 
     @ApiOperation("获取菜单权限")
@@ -76,5 +103,27 @@ public class MenuController extends BaseController {
     public Result getMenuTree() {
         List<Menu> menus = menuService.getMenuTree();
         return Result.success(menus);
+    }
+
+    /**
+     * 分组实体校验
+     *
+     * @param menu 菜单
+     */
+    private void validMenu(Menu menu, boolean isUpdate) {
+        // 若是更新则判断id是否存在
+        if (isUpdate && StrUtil.isBlank(menu.getId())) {
+            throw new BadRequestException(Result.failed("id不能为空"));
+        }
+        // 分组实体校验
+        Class<?> clazzGroup;
+        if (menu.getType() == MenuType.DIRECTORY) {
+            clazzGroup = Menu.DirectoryValidGroup.class;
+        } else if (menu.getType() == MenuType.MENU) {
+            clazzGroup = Menu.MenuValidGroup.class;
+        } else {
+            clazzGroup = Menu.ButtonValidGroup.class;
+        }
+        ValidatorUtil.validateEntity(menu, clazzGroup);
     }
 }
