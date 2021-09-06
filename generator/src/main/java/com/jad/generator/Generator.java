b43eda3eb8adb0b2a132afcc104e6ce35fd0450c
cxxwl96 <cxxwl96@sina.com>
2021-09-06 22:32:11 +0800

    代码生成

---
+++ generator/src/main/java/com/jad/generator/Generator.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C), 2021-2021, jad
+ */
+
+package com.jad.generator;
+
+/**
+ * 代码生成
+ *
+ * @author cxxwl96
+ * @since 2021/9/6 22:30
+ */
+public class Generator {
+}
---
+++ generator/src/main/java/com/jad/generator/config/Config.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C), 2021-2021, jad
+ */
+
+package com.jad.generator.config;
+
+/**
+ * 代码生成配置类
+ *
+ * @author cxxwl96
+ * @since 2021/9/6 22:31
+ */
+public class Config {
+}
